98fa8d24b19515bc371e20a530ef199c4911b338
Prabhakar b k <prabhukattimani18@gmail.com>
2023-07-13 13:57:11 +0530

    dataTypes example

---
+++ Camera8.java
@@ -0,0 +1,20 @@
+public class Camera8 {
+    String brand ;
+    String model ;
+    String color;
+    public static void main(String[] args) {
+        String brand = "Canon";
+        String model = "EOS R5";
+        String color = "Black";
+        int megapixels = 45;
+        boolean isMirrorless = true;
+
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+        System.out.println("Megapixels: " + megapixels);
+        System.out.println("Is Mirrorless: " + isMirrorless);
+       // int megapixels = 45;
+        //boolean isMirrorless = true;
+    }
+}
