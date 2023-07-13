85223c5077745dc339c1da359176e34d1f916459
Prabhakar b k <prabhukattimani18@gmail.com>
2023-07-13 13:41:17 +0530

    adding array files

---
+++ AmazonPrime.java
@@ -0,0 +1,210 @@
+public class AmazonPrime {
+
+    //it is a container whch store group of similar type of data into a single reference or variable
+
+    // static String kannadaMovies[] = {"KGF Chapter 1", "Dia","Tagaru","Ulidavaru Kandante",
+    //     "Kirik Party", "Gantumoote", "RangiTaranga", "Love Mocktail","Dheera Rana Vikrama","Lucia", "Avane Srimannarayana",
+    //     "Mayabazar 2016","Ondu Motteya Kathe","Bell Bottom", "Sarkari Hi. Pra. Shaale, Kasaragodu",
+    //     "Mugulu Nage","Mungaru Male", "Rajakumara", "Simple Agi Ondh Love Story","Thithi", "Ugramm", "Shivalinga",
+    //     "Kendasampige", "Sakkathagavne","Gosi Gang", "Aa Karaala Ratri","Puta 109", "Rama Rama Re",
+    //     "Kavaludaari", "Gultoo" };
+
+    //  static String hindiMovies[] = {"Dilwale Dulhania Le Jayenge", "Sholay", "Kabhi Khushi Kabhie Gham",
+    //     "3 Idiots", "Lagaan", "Kuch Kuch Hota Hai", "Kahaani", "Dangal", "Gully Boy", "Queen",
+    //     "Uri: The Surgical Strike", "Andhadhun", "PK", "Dabangg", "Padmaavat",
+    //     "Kesari", "Bajrangi Bhaijaan", "Super 30", "Krrish", "Dil To Pagal Hai",
+    //     "Kal Ho Naa Ho", "Hum Aapke Hain Koun..!", "Dhoom", "Golmaal: Fun Unlimited",
+    //     "Ra.One"};
+
+    // static String englishMovies[] = {"The Shawshank Redemption", "The Godfather", "Pulp Fiction",
+    //     "The Dark Knight", "Fight Club", "Inception", "Interstellar", "The Matrix", "Forrest Gump", "Goodfellas",
+    //     "The Lord of the Rings: The Fellowship of the Ring", "The Avengers", "The Lion King", "Jurassic Park",
+    //     "The Terminator", "The Social Network", "Gladiator", "Braveheart", "Titanic", "The Prestige",
+    //     "Casino Royale", "Gravity", "Mad Max: Fury Road", "The Revenant",
+    //     "La La Land"};
+
+    // static String tamilMovies[] = {"Roja", "Mouna Ragam", "Nayakan", "Sivaji", "Vada Chennai",
+    //     "Kabali", "96", "Super Deluxe", "Kaithi", "Asuran",
+    //     "Pudhupettai", "Aayirathil Oruvan", "Vikram Vedha", "Peranbu", "Ratsasan",
+    //     "Visaranai", "Aruvi", "Parasite", "Iraivi", "Thani Oruvan",
+    //     "Rhythm", "Aadukalam", "Baahubali: The Beginning", "Baahubali: The Conclusion",
+    //     "Alaipayuthey"};
+
+    // static String malayalamMovies[] = {"Drishyam", "Premam", "Kumbalangi Nights", "Bangalore Days",
+    //     "Maheshinte Prathikaaram", "Charlie", "Angamaly Diaries", "Ayyappanum Koshiyum", "Virus", "Jallikattu",
+    //     "Ustad Hotel", "Pathemari", "Moothon", "Njan Prakashan", "Ee. Ma. Yau.",
+    //     "Thondimuthalum Driksakshiyum", "Kali", "Varathan", "Kammara Sambhavam", "Joseph",
+    //     "Take Off", "Amen", "C U Soon", "Lucifer",
+    //     "Vellimoonga"};
+
+    // static String teluguMovies[] = {"Baahubali: The Beginning", "Baahubali: The Conclusion", "Arjun Reddy",
+    //     "Jersey", "Rangasthalam", "Ala Vaikunthapurramuloo", "Bahubali", "Sarileru Neekevvaru", "Vakeel Saab", "Nuvvu Naaku Nachav",
+    //     "Athadu", "Pokiri", "Eega", "Magadheera", "Sye",
+    //     "Nannaku Prematho", "F2: Fun and Frustration", "Rang De", "Jathi Ratnalu", "Baadshah",
+    //     "Manam", "Maharshi", "Geetha Govindam", "iSmart Shankar",
+    //     "Evaru"};
+
+    public static void main(String[] args) {
+
+
+          String kannadaMovies[] = {"KGF Chapter 1", "Dia","Tagaru","Ulidavaru Kandante",
+        "Kirik Party", "Gantumoote", "RangiTaranga", "Love Mocktail","Dheera Rana Vikrama","Lucia", "Avane Srimannarayana",
+        "Mayabazar 2016","Ondu Motteya Kathe","Bell Bottom", "Sarkari Hi. Pra. Shaale, Kasaragodu",
+        "Mugulu Nage","Mungaru Male", "Rajakumara", "Simple Agi Ondh Love Story","Thithi", "Ugramm", "Shivalinga",
+        "Kendasampige", "Sakkathagavne","Gosi Gang", "Aa Karaala Ratri","Puta 109", "Rama Rama Re",
+        "Kavaludaari", "Gultoo" };
+
+      String hindiMovies[] = {"Dilwale Dulhania Le Jayenge", "Sholay", "Kabhi Khushi Kabhie Gham",
+        "3 Idiots", "Lagaan", "Kuch Kuch Hota Hai", "Kahaani", "Dangal", "Gully Boy", "Queen",
+        "Uri: The Surgical Strike", "Andhadhun", "PK", "Dabangg", "Padmaavat",
+        "Kesari", "Bajrangi Bhaijaan", "Super 30", "Krrish", "Dil To Pagal Hai",
+        "Kal Ho Naa Ho", "Hum Aapke Hain Koun..!", "Dhoom", "Golmaal: Fun Unlimited",
+        "Ra.One"};
+
+    String englishMovies[] = {"The Shawshank Redemption", "The Godfather", "Pulp Fiction",
+        "The Dark Knight", "Fight Club", "Inception", "Interstellar", "The Matrix", "Forrest Gump", "Goodfellas",
+        "The Lord of the Rings: The Fellowship of the Ring", "The Avengers", "The Lion King", "Jurassic Park",
+        "The Terminator", "The Social Network", "Gladiator", "Braveheart", "Titanic", "The Prestige",
+        "Casino Royale", "Gravity", "Mad Max: Fury Road", "The Revenant",
+        "La La Land"};
+
+     String tamilMovies[] = {"Roja", "Mouna Ragam", "Nayakan", "Sivaji", "Vada Chennai",
+        "Kabali", "96", "Super Deluxe", "Kaithi", "Asuran",
+        "Pudhupettai", "Aayirathil Oruvan", "Vikram Vedha", "Peranbu", "Ratsasan",
+        "Visaranai", "Aruvi", "Parasite", "Iraivi", "Thani Oruvan",
+        "Rhythm", "Aadukalam", "Baahubali: The Beginning", "Baahubali: The Conclusion",
+        "Alaipayuthey"};
+
+     String malayalamMovies[] = {"Drishyam", "Premam", "Kumbalangi Nights", "Bangalore Days",
+        "Maheshinte Prathikaaram", "Charlie", "Angamaly Diaries", "Ayyappanum Koshiyum", "Virus", "Jallikattu",
+        "Ustad Hotel", "Pathemari", "Moothon", "Njan Prakashan", "Ee. Ma. Yau.",
+        "Thondimuthalum Driksakshiyum", "Kali", "Varathan", "Kammara Sambhavam", "Joseph",
+        "Take Off", "Amen", "C U Soon", "Lucifer",
+        "Vellimoonga"};
+
+     String teluguMovies[] = {"Baahubali: The Beginning", "Baahubali: The Conclusion", "Arjun Reddy",
+        "Jersey", "Rangasthalam", "Ala Vaikunthapurramuloo", "Bahubali", "Sarileru Neekevvaru", "Vakeel Saab", "Nuvvu Naaku Nachav",
+        "Athadu", "Pokiri", "Eega", "Magadheera", "Sye",
+        "Nannaku Prematho", "F2: Fun and Frustration", "Rang De", "Jathi Ratnalu", "Baadshah",
+        "Manam", "Maharshi", "Geetha Govindam", "iSmart Shankar",
+        "Evaru"};
+    
+        getKannadaMovies(kannadaMovies);
+        // for(int i=0; i<=kannadaMovies.length-1;){
+
+        //     System.out.println(kannadaMovies[i]);
+        //     i++;
+
+        // }
+        // System.out.println("List of kannada movies : ");
+        // System.out.println(kannadaMovies[0] + " " + kannadaMovies[1] + " " + kannadaMovies[2] + " " + kannadaMovies[3] + " " + kannadaMovies[4] + " " + kannadaMovies[5] + " " + kannadaMovies[6] + " " + kannadaMovies[7] + " " + kannadaMovies[8] + " " + 
+        // kannadaMovies[9] + " " + kannadaMovies[10] + " " + kannadaMovies[11] + " " + kannadaMovies[12] + " " + kannadaMovies[13] + " " + kannadaMovies[14] + " " + kannadaMovies[15] + " " + kannadaMovies[16] + " " + kannadaMovies[17] + " " + kannadaMovies[18] 
+        // + " " + kannadaMovies[19] + " " + kannadaMovies[20] + " " + kannadaMovies[21] + " " + kannadaMovies[22] + " " + kannadaMovies[23] + " " + kannadaMovies[24] +"\n");
+        
+         getHindiMovies(hindiMovies);
+        //  for(int i=0; i<=hindiMovies.length-1;){
+
+        //     System.out.println(hindiMovies[i]);
+        //     i++;
+
+        // }
+        // System.out.println("List of hiidi movies : ");
+        // System.out.println(hindiMovies[0] + " " + hindiMovies[1] + " " + hindiMovies[2] + " " + hindiMovies[3] + " " + hindiMovies[4] + " " + hindiMovies[5] + " " + hindiMovies[6] + " " + hindiMovies[7] + " " +
+        //  hindiMovies[8] + " " + hindiMovies[9] + " " + hindiMovies[10] + " " + hindiMovies[11] + " " + hindiMovies[12] + " " + hindiMovies[13] + " " + hindiMovies[14] + " " + hindiMovies[15] + " " + hindiMovies[16] + " " + 
+        //  hindiMovies[17] + " " + hindiMovies[18] + " " + hindiMovies[19] + " " + hindiMovies[20] + " " + hindiMovies[21] + " " + hindiMovies[22] + " " + hindiMovies[23] + " " + hindiMovies[24]);
+        
+
+        //  for(int i=0; i<=englishMovies.length-1;){
+
+        //     System.out.println(englishMovies[i]);
+        //     i++;
+
+        // }
+        getEnglishMovies(englishMovies);
+
+        //  for(String englishMovie:englishMovies){
+
+        //     System.out.println(englishMovie);
+            
+
+        // }
+        // System.out.println("List of 25 popular english movies : ");
+        // System.out.println(englishMovies[0] + " " + englishMovies[1] + " " + englishMovies[2] + " " + englishMovies[3] + " " + englishMovies[4] + " " + englishMovies[5] + " " + englishMovies[6] + " " + englishMovies[7] + " " + englishMovies[8] + " " + englishMovies[9] 
+        // + " " + englishMovies[10] + " " + englishMovies[11] + " " + englishMovies[12] + " " + englishMovies[13] + " " + englishMovies[14] + " " + englishMovies[15] + " " + englishMovies[16] + " " + 
+        // englishMovies[17] + " " + englishMovies[18] + " " + englishMovies[19] + " " + englishMovies[20] + " " + englishMovies[21] + " " + englishMovies[22] + " " + englishMovies[23] + " " + englishMovies[24] +"\n");
+       
+        //  for(int i=0; i<=teluguMovies.length-1;){
+
+        //     System.out.println(teluguMovies[i]);
+        //     i++;
+
+        // }
+        getTeluguMovies(teluguMovies);
+        //  for(String teluguMovie:teluguMovies){
+
+        //     System.out.println(teluguMovie);
+            
+
+        // }
+        // System.out.println("List of telugu movies : ");
+        // System.out.println(teluguMovies[0] + " " + teluguMovies[1] + " " + teluguMovies[2] + " " + teluguMovies[3] + " " + teluguMovies[4] + " " + teluguMovies[5] + " " + teluguMovies[6] + " " + 
+        // teluguMovies[7] + " " + teluguMovies[8] + " " + teluguMovies[9] + " " + teluguMovies[10] + " " + teluguMovies[11] + " " + teluguMovies[12] + " " + teluguMovies[13] + " " + teluguMovies[14] + " " + teluguMovies[15] + " " + teluguMovies[16] + " " + teluguMovies[17] + " " + 
+        // teluguMovies[18] + " " + teluguMovies[19] + " " + teluguMovies[20] + " " + teluguMovies[21] + " " + teluguMovies[22] + " " + teluguMovies[23] + " " + teluguMovies[24] +"\n");
+  
+        //  for(int i=0; i<=malayalamMovies.length-1;){
+
+        //     System.out.println(malayalamMovies[i]);
+        //     i++;
+
+        // }
+        getTamilMovies(tamilMovies);
+       getMalayalamMovies(malayalamMovies);
+        // for(String malayalamMovie:malayalamMovies){
+        //      System.out.println(malayalamMovie);
+        // }
+        
+        // System.out.println("List of 25 malayalm movies : ");
+        // System.out.println(malayalamMovies[0] + " " + malayalamMovies[1] + " " + malayalamMovies[2] + " " + malayalamMovies[3] + " " + malayalamMovies[4] + " " + malayalamMovies[5] + " " + malayalamMovies[6] + " " + malayalamMovies[7] + " " +
+        //  malayalamMovies[8] + " " + malayalamMovies[9] + " " + malayalamMovies[10] + " " + malayalamMovies[11] + " " + malayalamMovies[12] + " " + malayalamMovies[13] + " " + malayalamMovies[14] + " " + malayalamMovies[15] + " " + malayalamMovies[16] + " " + malayalamMovies[17] + " " + malayalamMovies[18] + " " + 
+        //  malayalamMovies[19] + " " + malayalamMovies[20] + " " + malayalamMovies[21] + " " + malayalamMovies[22] + " " + malayalamMovies[23] + " " + malayalamMovies[24] +"\n");
+    
+    }
+
+
+    public static void getKannadaMovies(String kannadaMovies[]) {
+    for(String  kannada:kannadaMovies){
+            System.out.println(kannada);
+        }
+    }
+
+    public static void getHindiMovies(String hindiMovies[]) {
+    for(String  hindi:hindiMovies){
+            System.out.println(hindi);
+        }
+    }
+
+    public static void getEnglishMovies(String englishMovies[]) {
+    for(String  english:englishMovies){
+            System.out.println(english);
+        }
+    }
+
+    public static void getTeluguMovies(String teluguMovies[]) {
+    for(String  telugu:teluguMovies){
+            System.out.println(telugu);
+        }
+    }
+
+    public static void getTamilMovies(String tamilMovies[]) {
+    for(String  tamil:tamilMovies){
+            System.out.println(tamil);
+        }
+    }
+
+    public static void getMalayalamMovies(String malayalamMovies[]) {
+    for(String  malayalam:malayalamMovies){
+            System.out.println(malayalam);
+        }
+    }
+}  
+    
