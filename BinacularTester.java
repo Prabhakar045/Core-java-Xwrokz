deb23f8e432cef213ba14ebeca2fbf76673ef08c
Prabhakar b k <prabhukattimani18@gmail.com>
2023-07-24 19:32:46 +0530

    constructor example

---
+++ BinacularTester.java
@@ -0,0 +1,13 @@
+public class BinacularTester {
+    
+    public static void main(String[] args) {
+        
+        Binacular binacular = new Binacular("convex", "Cannon" );
+
+        // // binacular.typoOfLens="convex";
+        // binacular.brandName="Sony";
+        // binacular.price=1000.00;
+
+        System.out.println( binacular.typoOfLens+"\n"+binacular.brandName+" \n"+binacular.price);
+    }
+}
