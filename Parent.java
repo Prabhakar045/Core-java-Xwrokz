2ca91718fed7feb5e6396fd8092e3981ad35c388
Prabhakar b k <prabhukattimani18@gmail.com>
2023-09-01 20:19:24 +0530

    2nd class polymorphism

---
+++ Parent.java
@@ -0,0 +1,11 @@
+package com.xworkz.ParentchildPolymorphism;
+
+public class Parent {
+    public void business() {
+        System.out.println("Parent's business method");
+    }
+
+    public void videoCreator(){
+
+    }
+}
